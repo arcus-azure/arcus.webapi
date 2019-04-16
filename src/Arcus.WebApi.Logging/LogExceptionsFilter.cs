50ea5ba30e43819a30901dfdda46c616aea48376
Frederik Gheysels <frederik.gheysels@codit.eu>
2019-04-16 17:26:48 +0200

    Add LogExceptionsFilter class
    This class inherits from ExceptionFilterAttribute

---
+++ src/Arcus.WebApi.Logging/LogExceptionsFilter.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+
+namespace Arcus.WebApi.Logging
+{
+    public class LogExceptionsFilter : ExceptionFilterAttribute
+    {
+        private readonly ILogger _logger;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LogExceptionsFilter"/> class.
+        /// </summary>
+        public LogExceptionsFilter(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public override void OnException(ExceptionContext context)
+        {
+            _logger.LogCritical(context.Exception, context.Exception.Message);
+
+            context.HttpContext.Response.StatusCode = 500;
+        }
+    }
+}
