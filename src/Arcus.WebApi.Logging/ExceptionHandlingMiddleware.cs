270ef317283a3ed7f9dcc875894f3345db0b5557
Frederik Gheysels <frederik.gheysels@codit.eu>
2019-04-16 22:42:11 +0200

    Add ExceptionHandlingMiddleware
    This middleware can be used to log unhandled exceptions that occur during execution of ASP.NET requests.

---
+++ src/Arcus.WebApi.Logging/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Arcus.WebApi.Logging
+{
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionHandlingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context, ILoggerFactory loggerFactory)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                HandleException(context, ex, loggerFactory);
+            }
+        }
+
+        private static void HandleException(HttpContext context, Exception ex, ILoggerFactory loggerFactory)
+        {
+            var logger = loggerFactory.CreateLogger("");
+            logger.LogCritical(ex, ex.Message);
+
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError; ;
+        }
+    }
+}
