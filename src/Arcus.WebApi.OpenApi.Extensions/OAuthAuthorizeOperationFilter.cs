c91c861a25325a736ec35148d90a0cb72965ab6d
Frederik Gheysels <frederik.gheysels@telenet.be>
2019-07-15 16:24:53 +0200

    Adhere to defensive programming techniques
    Add an extra check to avoid null-reference exceptions when referencing the DeclaringType of a method.

--- src/Arcus.WebApi.OpenApi.Extensions/OAuthAuthorizeOperationFilter.cs
+++ src/Arcus.WebApi.OpenApi.Extensions/OAuthAuthorizeOperationFilter.cs
@@ -36,8 +36,9 @@ namespace Arcus.WebApi.OpenApi.Extensions
         {
             var hasAuthorize = context.MethodInfo.GetCustomAttributes(true).OfType<AuthorizeAttribute>().Any() ||
                                (
-                                   context.MethodInfo.DeclaringType.GetCustomAttributes(true).OfType<AuthorizeAttribute>().Any() &&
-                                   context.MethodInfo.GetCustomAttributes(false).OfType<AllowAnonymousAttribute>().Any() == false
+                                    context.MethodInfo.DeclaringType != null &&
+                                    context.MethodInfo.DeclaringType.GetCustomAttributes(true).OfType<AuthorizeAttribute>().Any() &&
+                                    context.MethodInfo.GetCustomAttributes(false).OfType<AllowAnonymousAttribute>().Any() == false
                                );
 
             if (hasAuthorize)
