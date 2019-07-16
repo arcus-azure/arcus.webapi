44961215ccbe4ee039f89af2efd2682532200d09
Frederik Gheysels <frederik.gheysels@telenet.be>
2019-07-16 16:07:13 +0200

    Adding Arcus.WebApi.OpenApi.Extensions project (#46)
    * Add Arcus.WebApi.OpenApi.Extensions project
    
    * Add OAuthAuthorizeOperationFilter
    
    * Added packaging tags
    
    * Adding documentation on arcus.openapi.extensions
    
    Providing documentation on usage of the OAuthAuthorizeOperationFilter class
    
    * Update docs/features/openapi.securitydefinitions.md
    
    Co-Authored-By: Tom Kerkhove <kerkhove.tom@gmail.com>
    
    * Fixed type-name in documentation
    
    * Update docs/features/openapi.securitydefinitions.md
    
    Co-Authored-By: Tom Kerkhove <kerkhove.tom@gmail.com>
    
    * Update docs/index.md
    
    Co-Authored-By: Tom Kerkhove <kerkhove.tom@gmail.com>
    
    * Update docs/index.md
    
    Co-Authored-By: Tom Kerkhove <kerkhove.tom@gmail.com>
    
    * Renamed openapi.securitydefinitions.md to openapi-securitydefinitions.md
    
    Renaming this file is required to be in-sync with the Markdown contents
    of the index.md file, otherwise we would have broken links.
    
    * Provide XML comments on OAuthAuthorizeOperationFilter.Apply
    
    * XML comments on OAuthAuthorizeOperationFilter ctor
    
    * Add validation of constructor arguments in OAuthAuthorizeOperationFilter
    
    * Adhere to defensive programming techniques
    
    Add an extra check to avoid null-reference exceptions when referencing the DeclaringType of a method.
    
    * Update openapi.extensions documentation to adhere to Arcus guidelines
    
    * Fixed documentation on OAuthAuthorizeOperationFilter

---
+++ src/Arcus.WebApi.OpenApi.Extensions/OAuthAuthorizeOperationFilter.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.AspNetCore.Authorization;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Collections.Generic;
+using System.Linq;
+using GuardNet;
+
+namespace Arcus.WebApi.OpenApi.Extensions
+{
+    /// <summary>
+    /// A Swashbuckle operation filter that adds OAuth security definitions to authorized API operations.
+    /// </summary>
+    public class OAuthAuthorizeOperationFilter : IOperationFilter
+    {
+        private readonly IEnumerable<string> _scopes;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OAuthAuthorizeOperationFilter"/> class.
+        /// </summary>
+        /// <param name="scopes">A list of API scopes that is defined for the API that must be documented.</param>
+        /// <remarks>It is not possible right now to document the scopes on a fine grained operation-level.</remarks>
+        public OAuthAuthorizeOperationFilter(IEnumerable<string> scopes)
+        {
+            Guard.NotNull(scopes, nameof(scopes));
+            Guard.For<ArgumentException>(() => scopes.Any(String.IsNullOrWhiteSpace), "The list of scopes cannot contain an empty scope");
+            _scopes = scopes;
+        }
+
+        /// <summary>
+        /// Applies the OperationFilter to the API <paramref name="operation"/>.
+        /// </summary>
+        /// <param name="operation">The <see cref="Operation"/> instance on which the OperationFilter must be applied.</param>
+        /// <param name="context">Provides meta-information on the <paramref name="operation"/> instance.</param>
+        public void Apply(Operation operation, OperationFilterContext context)
+        {
+            var hasAuthorize = context.MethodInfo.GetCustomAttributes(true).OfType<AuthorizeAttribute>().Any() ||
+                               (
+                                    context.MethodInfo.DeclaringType != null &&
+                                    context.MethodInfo.DeclaringType.GetCustomAttributes(true).OfType<AuthorizeAttribute>().Any() &&
+                                    context.MethodInfo.GetCustomAttributes(false).OfType<AllowAnonymousAttribute>().Any() == false
+                               );
+
+            if (hasAuthorize)
+            {
+                if (operation.Responses.ContainsKey("401") == false)
+                {
+                    operation.Responses.Add("401", new Response { Description = "Unauthorized" });
+                }
+
+                if (operation.Responses.ContainsKey("403") == false)
+                {
+                    operation.Responses.Add("403", new Response { Description = "Forbidden" });
+                }
+
+                operation.Security = new List<IDictionary<string, IEnumerable<string>>>
+                {
+                    new Dictionary<string, IEnumerable<string>>
+                    {
+                        { "oauth2", _scopes }
+                    }
+                };
+            }
+        }
+    }
+}
