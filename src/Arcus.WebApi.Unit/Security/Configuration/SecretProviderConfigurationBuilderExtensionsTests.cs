5147f442fee67d896c6ccf4a0528c62a55f2dac3
Stijn Moreels <--global>
2019-07-31 10:53:30 +0200

    FEAT: add 'ISecretProvider' with '.AddAzureKeyVault' configuration builder

---
+++ src/Arcus.WebApi.Unit/Security/Configuration/SecretProviderConfigurationBuilderExtensionsTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Configuration;
+using Xunit;
+using Arcus.WebApi.Security.Configuration;
+using System.Collections.Generic;
+
+namespace Arcus.WebApi.Unit.Security.Configuration
+{
+    public class SecretProviderConfigurationBuilderExtensionsTests
+    {
+        [Fact]
+        public void AddKeyVault_Accesses_SecretProvider_For_Secret_Values_From_Configuration_Tokens()
+        {
+            // Arrange
+            const string configurationKey = "ConnectionString";
+            const string expected = "connection to somewhere";
+
+            var stubProvider = new InMemorySecretProvider((configurationKey, expected));
+
+            var configuration =
+                new ConfigurationBuilder()
+                    .AddInMemoryCollection(new [] { new KeyValuePair<string, string>(configurationKey, "#{ConnectionString}#") })
+                    .AddAzureKeyVault(stubProvider)
+                    .Build();
+
+            // Act
+            IConfigurationSection section = configuration.GetSection(configurationKey);
+
+            // Assert
+            Assert.Equal(expected, section.Value);
+        }
+    }
+}
