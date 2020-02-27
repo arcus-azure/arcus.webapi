91bfd0f6159f65b5aee83b603ccc6838fafcb5c4
Tom Kerkhove <kerkhove.tom@gmail.com>
2020-02-27 19:16:07 +0100

    Provide managed identity foundation

---
+++ src/Arcus.WebApi.Security.Authorization/Jwt/IJwtTokenReader.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace Arcus.WebApi.Security.Authorization.Jwt
+{
+    public interface IJwtTokenReader
+    {
+        /// <summary>
+        ///     Validates if the token is considered valid
+        /// </summary>
+        /// <param name="token">JWT token</param>
+        Task<bool> IsValidToken(string token);
+    }
+}
