57d698fe0a920004743d08137cacb4420ff7f340
Tom Kerkhove <kerkhove.tom@gmail.com>
2020-02-28 10:26:11 +0100

    Provide Azure Managed Identity authorization foundation (#121)
    * Provide managed identity foundation
    
    * Update dependencies
    
    * Improve NuGet package info
    
    * Provide tests
    
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>

---
+++ src/Arcus.WebApi.Security.Authorization/AzureManagedIdentityAuthorizationFilter.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks;
+using Arcus.WebApi.Security.Authorization.Jwt;
+using GuardNet;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Arcus.WebApi.Security.Authorization
+{
+    public class AzureManagedIdentityAuthorizationFilter : IAsyncAuthorizationFilter
+    {
+        private const string DefaultHeaderName = "x-managed-identity-token";
+
+        private readonly string _headerName;
+        private readonly IJwtTokenReader _jwtTokenReader;
+
+        public AzureManagedIdentityAuthorizationFilter(IJwtTokenReader jwtTokenReader)
+            : this(DefaultHeaderName, jwtTokenReader)
+        {
+        }
+
+        public AzureManagedIdentityAuthorizationFilter(string headerName, IJwtTokenReader jwtTokenReader)
+        {
+            Guard.NotNullOrEmpty(headerName, nameof(headerName));
+            Guard.NotNull(jwtTokenReader, nameof(jwtTokenReader));
+
+            _headerName = headerName;
+            _jwtTokenReader = jwtTokenReader;
+        }
+
+        public virtual async Task OnAuthorizationAsync(AuthorizationFilterContext context)
+        {
+            var jwtString = context.HttpContext.Request.Headers[_headerName].ToString();
+
+            if (string.IsNullOrWhiteSpace(jwtString) || !await _jwtTokenReader.IsValidToken(jwtString))
+            {
+                context.Result = new UnauthorizedObjectResult("Unauthorized because of wrong JWT MSI token.");
+            }
+        }
+    }
+}
---
+++ src/Arcus.WebApi.Security.Authorization/Jwt/JwtTokenReader.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Threading.Tasks;
+using GuardNet;
+using Microsoft.IdentityModel.Protocols;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Arcus.WebApi.Security.Authorization.Jwt
+{
+    public class JwtTokenReader : IJwtTokenReader
+    {
+        private const string MicrosoftDiscoveryEndpoint =
+            "https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration";
+
+        private readonly string _applicationId;
+        private readonly JwtSecurityTokenHandler _handler;
+        private readonly ConfigurationManager<OpenIdConnectConfiguration> _configManager;
+
+        public JwtTokenReader(string applicationId)
+        {
+            Guard.NotNullOrWhitespace(applicationId, nameof(applicationId));
+
+            _applicationId = applicationId;
+
+            _handler = new JwtSecurityTokenHandler();
+            _configManager = new ConfigurationManager<OpenIdConnectConfiguration>(MicrosoftDiscoveryEndpoint,
+                new OpenIdConnectConfigurationRetriever());
+        }
+
+        /// <summary>
+        ///     Validates if the token is considered valid
+        /// </summary>
+        /// <param name="token">JWT token</param>
+        public async Task<bool> IsValidToken(string token)
+        {
+            OpenIdConnectConfiguration config = await _configManager.GetConfigurationAsync();
+
+            var validationParameters = new TokenValidationParameters
+            {
+                ValidateAudience = true,
+                ValidAudience = _applicationId,
+                ValidateIssuer = false,
+                IssuerSigningKeys = config.SigningKeys,
+                ValidateLifetime = true
+            };
+
+            SecurityToken jwtToken;
+            try
+            {
+                _handler.ValidateToken(token, validationParameters, out jwtToken);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
