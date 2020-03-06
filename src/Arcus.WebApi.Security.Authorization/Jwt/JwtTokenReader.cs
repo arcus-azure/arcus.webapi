351a11499926dee802b4cd8511d3ad2adc0681ea
Tom Kerkhove <kerkhove.tom@gmail.com>
2020-03-06 08:43:15 +0100

    Provide configuration options for Managed Identity authorization… (#129)
    * Provide configuration options for Managed Identity authorization validation
    
    * Move parameters
    
    * IsValidToken -> IsValidTokenAsync
    
    * Initialize on field
    
    * Fix test
    
    * Move to var
    
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>
    
    * Capture in var
    
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>

--- src/Arcus.WebApi.Security.Authorization/Jwt/JwtTokenReader.cs
+++ src/Arcus.WebApi.Security.Authorization/Jwt/JwtTokenReader.cs
@@ -14,29 +14,55 @@ namespace Arcus.WebApi.Security.Authorization.Jwt
             "https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration";
 
         private readonly string _applicationId;
-        private readonly JwtSecurityTokenHandler _handler;
+        private readonly TokenValidationParameters _tokenValidationParameters;
+        private readonly JwtSecurityTokenHandler _handler = new JwtSecurityTokenHandler();
         private readonly ConfigurationManager<OpenIdConnectConfiguration> _configManager;
 
+        /// <summary>
+        ///     Constructor
+        /// </summary>
+        /// <remarks>Uses Microsoft OpenId connect discovery endpoint</remarks>
+        /// <param name="applicationId">Azure AD Application used as audience to validate against</param>
         public JwtTokenReader(string applicationId)
         {
             Guard.NotNullOrWhitespace(applicationId, nameof(applicationId));
 
             _applicationId = applicationId;
+            _configManager = new ConfigurationManager<OpenIdConnectConfiguration>(MicrosoftDiscoveryEndpoint, new OpenIdConnectConfigurationRetriever());
+        }
 
-            _handler = new JwtSecurityTokenHandler();
-            _configManager = new ConfigurationManager<OpenIdConnectConfiguration>(MicrosoftDiscoveryEndpoint,
-                new OpenIdConnectConfigurationRetriever());
+        /// <summary>
+        ///     Constructor
+        /// </summary>
+        /// <remarks>Uses Microsoft OpenId connect discovery endpoint</remarks>
+        /// <param name="tokenValidationParameters">Collection of parameters to influence how the token validation is done</param>
+        public JwtTokenReader(TokenValidationParameters tokenValidationParameters) : this(tokenValidationParameters, MicrosoftDiscoveryEndpoint)
+        {
+        }
+
+        /// <summary>
+        ///     Constructor
+        /// </summary>
+        /// <param name="openIdConnectDiscoveryUri">Uri of an OpenId connect endpoint for discovering the configuration</param>
+        /// <param name="tokenValidationParameters">Collection of parameters to influence how the token validation is done</param>
+        public JwtTokenReader(TokenValidationParameters tokenValidationParameters, string openIdConnectDiscoveryUri)
+        {
+            Guard.NotNullOrWhitespace(openIdConnectDiscoveryUri, nameof(openIdConnectDiscoveryUri));
+            Guard.NotNull(tokenValidationParameters, nameof(tokenValidationParameters));
+
+            _tokenValidationParameters = tokenValidationParameters;
+            _configManager = new ConfigurationManager<OpenIdConnectConfiguration>(openIdConnectDiscoveryUri, new OpenIdConnectConfigurationRetriever());
         }
 
         /// <summary>
         ///     Validates if the token is considered valid
         /// </summary>
         /// <param name="token">JWT token</param>
-        public async Task<bool> IsValidToken(string token)
+        public async Task<bool> IsValidTokenAsync(string token)
         {
             OpenIdConnectConfiguration config = await _configManager.GetConfigurationAsync();
 
-            var validationParameters = new TokenValidationParameters
+            var validationParameters = _tokenValidationParameters ?? new TokenValidationParameters
             {
                 ValidateAudience = true,
                 ValidAudience = _applicationId,
--- src/Arcus.WebApi.Unit/Security/Authorization/JwtTokenReaderTests.cs
+++ src/Arcus.WebApi.Unit/Security/Authorization/JwtTokenReaderTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Arcus.WebApi.Security.Authorization.Jwt;
+using Microsoft.IdentityModel.Tokens;
 using Xunit;
 
 namespace Arcus.WebApi.Unit.Security.Authorization
@@ -24,6 +25,54 @@ namespace Arcus.WebApi.Unit.Security.Authorization
 
             // Act
             var jwtTokenReader = new JwtTokenReader(applicationId);
+
+            // Assert
+            Assert.NotNull(jwtTokenReader);
+        }
+
+        [Fact]
+        public void Constructor_WithTokenValidationParameters_Succeeds()
+        {
+            // Arrange
+            var tokenValidationParameters = new TokenValidationParameters();
+
+            // Act
+            var jwtTokenReader = new JwtTokenReader(tokenValidationParameters);
+
+            // Assert
+            Assert.NotNull(jwtTokenReader);
+        }
+
+        [Fact]
+        public void Constructor_WithTokenValidationParametersButWithoutDiscoveryEndpoint_ThrowsException()
+        {
+            // Arrange
+            var tokenValidationParameters = new TokenValidationParameters();
+            string openIdConnectDiscoveryUri = string.Empty;
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => new JwtTokenReader(tokenValidationParameters, openIdConnectDiscoveryUri));
+        }
+
+        [Fact]
+        public void Constructor_WithoutTokenValidationParameters_Succeeds()
+        {
+            // Arrange
+            TokenValidationParameters tokenValidationParameters = null;
+
+            // Act & Assert
+            Assert.Throws<ArgumentNullException>(() => new JwtTokenReader(tokenValidationParameters));
+        }
+
+        [Fact]
+        public void Constructor_WithTokenValidationParametersAndDiscoveryEndpoint_Succeeds()
+        {
+            // Arrange
+            var tokenValidationParameters = new TokenValidationParameters();
+            string openIdConnectDiscoveryUri = "https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration";
+
+            // Act
+            var jwtTokenReader = new JwtTokenReader(tokenValidationParameters, openIdConnectDiscoveryUri);
 
             // Assert
             Assert.NotNull(jwtTokenReader);
