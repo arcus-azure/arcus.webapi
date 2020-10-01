199bfe0bd4d69ef2f286c209e9e1001073f39f01
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2020-10-01 07:44:31 +0200

    Feature - add version tracking middleware (#183)
    * Feature - add version tracking middleware
    
    * Update logging.md
    
    * Update src/Arcus.WebApi.Logging/Correlation/CorrelationMiddleware.cs
    
    Co-authored-by: Tom Kerkhove <kerkhove.tom@gmail.com>
    
    * Update docs/preview/features/logging.md
    
    Co-authored-by: Tom Kerkhove <kerkhove.tom@gmail.com>
    
    * Update docs/preview/features/logging.md
    
    Co-authored-by: Frederik Gheysels <frederik.gheysels@telenet.be>
    
    * Update docs/preview/features/logging.md
    
    Co-authored-by: Frederik Gheysels <frederik.gheysels@telenet.be>
    
    * Update logging.md
    
    * Update VersionTrackingMiddleware.cs
    
    * Update IApplicationBuilderExtensions.cs
    
    Co-authored-by: Tom Kerkhove <kerkhove.tom@gmail.com>
    Co-authored-by: Frederik Gheysels <frederik.gheysels@telenet.be>

---
+++ src/Arcus.WebApi.Logging/VersionTrackingOptions.cs
@@ -0,0 +1,25 @@
+﻿using GuardNet;
+
+namespace Arcus.WebApi.Logging
+{
+    /// <summary>
+    /// Represents the user-configurable options to control how the <see cref="VersionTrackingMiddleware"/> should track the current application version in the response.
+    /// </summary>
+    public class VersionTrackingOptions
+    {
+        private string _headerName = "X-Version";
+
+        /// <summary>
+        /// Gets or sets the header name on which the current application version should be added.
+        /// </summary>
+        public string HeaderName
+        {
+            get => _headerName;
+            set
+            {
+                Guard.NotNullOrWhitespace(value, nameof(value), "Requires a non-blank header name to add the current application version to the response");
+                _headerName = value;
+            }
+        }
+    }
+}
