812115c9a51c23e68c78d9cd2a2f083cc29ef80c
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2021-03-04 09:32:45 +0100

    pr-style: remove blank lines from echo controller

--- src/Arcus.WebApi.Tests.Unit/Hosting/EchoController.cs
+++ src/Arcus.WebApi.Tests.Unit/Hosting/EchoController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace Arcus.WebApi.Tests.Unit.Hosting
 {
@@ -24,6 +20,5 @@ namespace Arcus.WebApi.Tests.Unit.Hosting
         {
             return Ok(body);
         }
-
     }
 }
