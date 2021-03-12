5756da6e1efb948e772e5dd89def2b435d576afd
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2021-03-12 08:14:13 +0100

    Update src/Arcus.WebApi.Logging/RequestTrackingMiddleware.cs
    Co-authored-by: Tom Kerkhove <kerkhove.tom@gmail.com>

--- src/Arcus.WebApi.Logging/RequestTrackingMiddleware.cs
+++ src/Arcus.WebApi.Logging/RequestTrackingMiddleware.cs
@@ -207,7 +207,7 @@ namespace Arcus.WebApi.Logging
                 return sanitizedBody;
             }
                 
-            _logger.LogWarning("No {Target} body was found to be tracked", target);
+            _logger.LogTrace("No {Target} body was found to be tracked", target);
             return null;
         }
 
