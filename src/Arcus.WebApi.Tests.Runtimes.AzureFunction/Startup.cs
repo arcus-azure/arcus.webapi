811e42e2561de01bcd33ef3c0e233807bd5ad374
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2022-03-04 08:39:15 +0100

    feat: add http correlation extension for az func

--- src/Arcus.WebApi.Tests.Runtimes.AzureFunction/Startup.cs
+++ src/Arcus.WebApi.Tests.Runtimes.AzureFunction/Startup.cs
@@ -1,4 +1,6 @@
-﻿using Arcus.WebApi.Tests.Runtimes.AzureFunction;
+﻿using System;
+using Arcus.WebApi.Logging.Core.Correlation;
+using Arcus.WebApi.Tests.Runtimes.AzureFunction;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -14,7 +16,7 @@ namespace Arcus.WebApi.Tests.Runtimes.AzureFunction
         /// <param name="builder">The instance to build the registered services inside the functions app.</param>
         public override void Configure(IFunctionsHostBuilder builder)
         {
-            builder.AddHttpCorrelation();
+            builder.AddHttpCorrelation(configureOptions: (Action<HttpCorrelationInfoOptions>) null);
         }
     }
 }
