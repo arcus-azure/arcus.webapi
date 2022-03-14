9f8c02ba8b0b5a0d82679f2078b3bf179543d15f
Stijn Moreels <9039753+stijnmoreels@users.noreply.github.com>
2022-03-14 12:17:24 +0100

    feat: add http correlation extension for az func (#306)

--- src/Arcus.WebApi.Logging.AzureFunctions/Extensions/IServiceCollectionExtensions.cs
+++ src/Arcus.WebApi.Logging.AzureFunctions/Extensions/IServiceCollectionExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Arcus.Observability.Correlation;
+using Arcus.WebApi.Logging.Core.Correlation;
 using GuardNet;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 
@@ -18,9 +19,23 @@ namespace Microsoft.Extensions.DependencyInjection
         /// <param name="builder">The functions host builder containing the dependency injection services.</param>
         /// <param name="configureOptions">The function to configure additional options how the correlation works.</param>
         /// <exception cref="ArgumentNullException">Thrown when the <paramref name="builder"/> is <c>null</c>.</exception>
+        [Obsolete("Use the " + nameof(AddHttpCorrelation) + " method overload with the " + nameof(HttpCorrelationInfoOptions) + " instead")]
         public static IServiceCollection AddHttpCorrelation(this IFunctionsHostBuilder builder, Action<CorrelationInfoOptions> configureOptions = null)
         {
-            Guard.NotNull(builder, nameof(builder));
+            Guard.NotNull(builder, nameof(builder), "Requires a functions host builder instance to add the HTTP correlation services");
+
+            return builder.Services.AddHttpCorrelation(configureOptions);
+        }
+
+        /// <summary>
+        /// Adds operation and transaction correlation to the application.
+        /// </summary>
+        /// <param name="builder">The functions host builder containing the dependency injection services.</param>
+        /// <param name="configureOptions">The function to configure additional options how the correlation works.</param>
+        /// <exception cref="ArgumentNullException">Thrown when the <paramref name="builder"/> is <c>null</c>.</exception>
+        public static IServiceCollection AddHttpCorrelation(this IFunctionsHostBuilder builder, Action<HttpCorrelationInfoOptions> configureOptions)
+        {
+            Guard.NotNull(builder, nameof(builder), "Requires a function host builder instance to add the HTTP correlation services");
 
             return builder.Services.AddHttpCorrelation(configureOptions);
         }
---
+++ src/Arcus.WebApi.Tests.Unit/Logging/IFunctionsHostBuilderTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Arcus.Observability.Correlation;
+using Arcus.WebApi.Logging.Core.Correlation;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Xunit;
+
+namespace Arcus.WebApi.Tests.Unit.Logging
+{
+    // ReSharper disable once InconsistentNaming
+    public class IFunctionsHostBuilderTests
+    {
+        [Fact]
+        public void AddHttpCorrelation_WithoutOptions_RegistersDedicatedCorrelation()
+        {
+            // Arrange
+            var services = new ServiceCollection();
+            var builder = new Mock<IFunctionsHostBuilder>();
+            builder.Setup(build => build.Services).Returns(services);
+
+            // Act
+            builder.Object.AddHttpCorrelation((Action<HttpCorrelationInfoOptions>) null);
+
+            // Assert
+            IServiceProvider provider = services.BuildServiceProvider();
+            Assert.NotNull(provider.GetService<IHttpCorrelationInfoAccessor>());
+            Assert.NotNull(provider.GetService<ICorrelationInfoAccessor<CorrelationInfo>>());
+            Assert.NotNull(provider.GetService<ICorrelationInfoAccessor>());
+        }
+        
+        [Fact]
+        public void AddHttpCorrelation_WithHttpOptions_RegistersDedicatedCorrelation()
+        {
+            // Arrange
+            var services = new ServiceCollection();
+            var builder = new Mock<IFunctionsHostBuilder>();
+            builder.Setup(build => build.Services).Returns(services);
+
+            // Act
+            builder.Object.AddHttpCorrelation(options => options.UpstreamService.ExtractFromRequest = true);
+
+            // Assert
+            IServiceProvider provider = services.BuildServiceProvider();
+            Assert.NotNull(provider.GetService<IHttpCorrelationInfoAccessor>());
+            Assert.NotNull(provider.GetService<ICorrelationInfoAccessor<CorrelationInfo>>());
+            Assert.NotNull(provider.GetService<ICorrelationInfoAccessor>());
+        }
+    }
+}
