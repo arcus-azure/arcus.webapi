07f3e12426f2b98f797cd5a0761ac43351551414
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2022-10-12 13:35:50 +0200

    pr-fix: docker tests

--- src/Arcus.WebApi.Tests.Integration/Logging/AzureFunctionCorrelationDockerTests.cs
+++ src/Arcus.WebApi.Tests.Integration/Logging/AzureFunctionCorrelationDockerTests.cs
@@ -31,6 +31,8 @@ namespace Arcus.WebApi.Tests.Integration.Logging
         public AzureFunctionCorrelationDockerTests(ITestOutputHelper outputWriter)
         {
             _logger = new XunitTestLogger(outputWriter);
+
+            
         }
 
         public static IEnumerable<object[]> RunningAzureFunctionsDockerProjectUrls => new[]
@@ -45,7 +47,10 @@ namespace Arcus.WebApi.Tests.Integration.Logging
         {
             // Act
             _logger.LogInformation("GET -> '{Uri}'", url);
-            using (HttpResponseMessage response = await HttpClient.GetAsync(url))
+            var request = new HttpRequestMessage(HttpMethod.Get, url);
+            request.Headers.Remove("traceparent");
+
+            using (HttpResponseMessage response = await HttpClient.SendAsync(request))
             {
                 // Assert
                 _logger.LogInformation("{StatusCode} <- {Uri}", response.StatusCode, url);
@@ -57,7 +62,7 @@ namespace Arcus.WebApi.Tests.Integration.Logging
                 var content = JsonConvert.DeserializeAnonymousType(json, new { TransactionId = "", OperationId = "", OperationParentId = "" });
                 Assert.False(string.IsNullOrWhiteSpace(content.TransactionId), "Accessed 'X-Transaction-ID' cannot be blank");
                 Assert.False(string.IsNullOrWhiteSpace(content.OperationId), "Accessed 'X-Operation-ID' cannot be blank");
-                Assert.NotNull(content.OperationParentId);
+                Assert.Null(content.OperationParentId);
 
                 Assert.Equal(correlationId, content.TransactionId);
             }
@@ -80,7 +85,7 @@ namespace Arcus.WebApi.Tests.Integration.Logging
                 _logger.LogInformation("{StatusCode} <- {Uri}", response.StatusCode, url);
                 Assert.Equal(HttpStatusCode.OK, response.StatusCode);
 
-                string actual = GetResponseHeader(response, TransactionIdHeaderName);
+                string actual = GetResponseHeader(response, "traceparent");
                 Assert.Equal(expected, actual);
             }
         }
@@ -92,7 +97,7 @@ namespace Arcus.WebApi.Tests.Integration.Logging
             // Arrange
             string expected = BogusGenerator.Random.Hexadecimal(16, prefix: null);
             var request = new HttpRequestMessage(HttpMethod.Get, url);
-            request.Headers.Add(UpstreamServiceHeaderName, $"00-4b1c0c8d608f57db7bd0b13c88ef865e-{expected}-00");
+            request.Headers.Add("traceparent", $"00-4b1c0c8d608f57db7bd0b13c88ef865e-{expected}-00");
 
             // Act
             _logger.LogInformation("GET -> '{Uri}'", url);
@@ -102,7 +107,7 @@ namespace Arcus.WebApi.Tests.Integration.Logging
                 _logger.LogInformation("{StatusCode} <- {Uri}", response.StatusCode, url);
                 Assert.Equal(HttpStatusCode.OK, response.StatusCode);
 
-                string actual = GetResponseHeader(response, UpstreamServiceHeaderName);
+                string actual = GetResponseHeader(response, "traceparent");
                 Assert.Equal(expected, actual);
             }
         }
