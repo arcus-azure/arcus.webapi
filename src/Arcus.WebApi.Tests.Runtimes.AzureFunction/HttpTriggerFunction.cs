6d9599a7664c1486beab675c9db0365bf97d5182
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2022-10-31 07:26:16 +0100

    pr-fix: diff set of deps

--- src/Arcus.WebApi.Tests.Runtimes.AzureFunction/HttpTriggerFunction.cs
+++ src/Arcus.WebApi.Tests.Runtimes.AzureFunction/HttpTriggerFunction.cs
@@ -7,7 +7,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
@@ -15,13 +14,15 @@ namespace Arcus.WebApi.Tests.Runtimes.AzureFunction
 {
     public class HttpTriggerFunction
     {
+        private readonly AzureFunctionsInProcessHttpCorrelation _correlationService;
         private readonly ILogger<HttpTriggerFunction> _logger;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="HttpTriggerFunction"/> class.
         /// </summary>
-        public HttpTriggerFunction(ILogger<HttpTriggerFunction> logger)
+        public HttpTriggerFunction(AzureFunctionsInProcessHttpCorrelation correlationService, ILogger<HttpTriggerFunction> logger)
         {
+            _correlationService = correlationService;
             _logger = logger;
         }
         
@@ -30,12 +31,11 @@ namespace Arcus.WebApi.Tests.Runtimes.AzureFunction
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            var correlationService = req.HttpContext.RequestServices.GetRequiredService<AzureFunctionsInProcessHttpCorrelation>();
-            using (HttpCorrelationResult result = correlationService.CorrelateHttpRequest())
+            using (HttpCorrelationResult result = _correlationService.CorrelateHttpRequest())
             {
                 if (result.IsSuccess)
                 {
-                    CorrelationInfo correlationInfo = correlationService.GetCorrelationInfo();
+                    CorrelationInfo correlationInfo = _correlationService.GetCorrelationInfo();
                     _logger.LogInformation("Gets the HTTP correlation: [OperationId={OperationId}, TransactionId={TransactionId}]", correlationInfo.OperationId, correlationInfo.TransactionId);
 
                     string json = JsonConvert.SerializeObject(correlationInfo);
