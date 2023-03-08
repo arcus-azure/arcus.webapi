f9065c5ab5f51bb238ccaf971c04ff254b6931f7
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2023-03-08 07:55:58 +0100

    pr-fix: solely rely on worker pkg

--- src/Arcus.WebApi.Logging.AzureFunctions/Extensions/IServiceCollectionExtensions.cs
+++ src/Arcus.WebApi.Logging.AzureFunctions/Extensions/IServiceCollectionExtensions.cs
@@ -91,7 +91,7 @@ namespace Microsoft.Extensions.DependencyInjection
                     return new AzureFunctionsInProcessHttpCorrelation(options, correlationInfoAccessor, logger);
                 });
 
-                services.AddLogging(logging => logging.AddApplicationInsights());
+                services.AddApplicationInsightsTelemetryWorkerService();
             }
 
             if (options.Format is HttpCorrelationFormat.Hierarchical)
