09b317223e0b25dd19ec58a0e6375d33ee0aa992
Stijn Moreels <9039753+stijnmoreels@users.noreply.github.com>
2025-07-31 08:55:35 +0200

    chore(net10.0): add support for .net 10.0 throughout

--- src/Arcus.WebApi.Security/Authentication/Certificates/CertificateAuthenticationFilter.cs
+++ src/Arcus.WebApi.Security/Authentication/Certificates/CertificateAuthenticationFilter.cs
@@ -1,17 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Security.Cryptography.X509Certificates;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Primitives;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace Arcus.WebApi.Security.Authentication.Certificates
 {
@@ -157,12 +158,16 @@ namespace Arcus.WebApi.Security.Authentication.Certificates
             try
             {
                 var headerValue = headerValues.ToString();
-                if (!String.IsNullOrWhiteSpace(headerValue) 
-                    && headerValue.Trim().Length % 4 == 0 
+                if (!String.IsNullOrWhiteSpace(headerValue)
+                    && headerValue.Trim().Length % 4 == 0
                     && Base64Regex.IsMatch(headerValue))
                 {
                     byte[] rawData = Convert.FromBase64String(headerValue);
+#if NET8_0
                     clientCertificate = new X509Certificate2(rawData);
+#else
+                    clientCertificate = X509CertificateLoader.LoadCertificate(rawData);
+#endif                    
                     return true;
                 }
 
@@ -185,7 +190,7 @@ namespace Arcus.WebApi.Security.Authentication.Certificates
             {
                 return;
             }
-            
+
             var telemetryContext = new Dictionary<string, object>
             {
                 ["EventType"] = "Security",
@@ -195,7 +200,7 @@ namespace Arcus.WebApi.Security.Authentication.Certificates
 
             if (responseStatusCode != null)
             {
-                telemetryContext["StatusCode"] = responseStatusCode.ToString(); 
+                telemetryContext["StatusCode"] = responseStatusCode.ToString();
             }
 
             logger.LogSecurityEvent("Authentication", telemetryContext);
